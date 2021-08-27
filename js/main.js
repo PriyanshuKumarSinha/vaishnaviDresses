1d23d512f6ace95107b789a394e5d9a2b3328449
PriyanshuKumarSinha <priyanshuk9066@gmail.com>
2021-08-27 17:01:26 +0530

    v1.0

--- js/main.js
+++ js/main.js
@@ -48,3 +48,55 @@ function hideMenu() {
     // crossIcon.style.display = 'none';
     // hamIcon.style.display = "contents";
 }
+
+let offerSliderImage = document.querySelector("#offerSliderImage");
+let imageNumber = 0;
+let offerImagesName = ['offerImage.png', 'offerimagebottom.jpg', 'download (1).jpg', 'download (2).jpg', 'download (3).jpg', 'download (4).jpg', 'download (5).jpg', 'download (6).jpg', 'download.jpg', 'images.jpg', 'images.png', 'images (1).jpg'];
+
+// set first image
+
+
+let sliderButtonsDiv = document.querySelector('.sliderButtons')
+
+// create buttons
+// sliderButtonsDiv.innerHTML += `<button>&nbsp;</button>`
+for (let i = 0; i < offerImagesName.length; i++) {
+    sliderButtonsDiv.innerHTML += `<button id = "${i}" onclick = "buttonClicked(this.id)">&nbsp;</button>`
+}
+
+changeImage();
+
+
+function changeImage() {
+    offerSliderImage.src = `./img/${offerImagesName[imageNumber]}`;
+    document.getElementById(String(imageNumber)).style.backgroundColor = 'rgb(184, 184, 184)';
+    if (imageNumber >= 1) {
+        document.getElementById(String(imageNumber - 1)).style.backgroundColor = 'rgb(228, 228, 228)';
+    }
+    else {
+        document.getElementById(String(offerImagesName.length - 1)).style.backgroundColor = 'rgb(228, 228, 228)';
+    }
+
+    if (imageNumber < offerImagesName.length - 1) {
+        imageNumber += 1;
+    }
+    else {
+        imageNumber = 0;
+    }
+
+    // console.log(imageNumber)
+}
+
+setInterval(changeImage, 8000);
+
+
+
+
+function buttonClicked(id) {
+    imageNumber = parseInt(id);
+    for (let i = 0; i < offerImagesName.length; i++) {
+        document.getElementById(String(i)).style.backgroundColor = 'rgb(228, 228, 228)';
+    }
+    changeImage();
+    // console.log(id);
+}
